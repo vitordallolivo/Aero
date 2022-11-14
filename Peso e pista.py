945bfdd39c729c6b36f9a1807c9657fae2b50a89
vitordallolivo <71341805+vitordallolivo@users.noreply.github.com>
2022-11-14 20:56:08 -0300

    Add files via upload

--- Peso e pista.py
+++ Peso e pista.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt # plotar os graficos
 sl01=[] # SL0 para helice 12x8
 sl02=[] # SL0 para helice 13x6.5
 sl03=[] # SL0 para helice 13x4
-
 w=[] # array com varios pesos
 
-# trações máximas para cada helices
-helicer=[15.30818*0.7,28.14509*0.7,23.76151*0.7] #12x8 , 13x6.5 , 13x4 RIMFIRE
+# 70% trações máximas para cada helices
+#helicer=[15.30818*0.7,28.14509*0.7,23.76151*0.7] #12x8 , 13x6.5 , 13x4 RIMFIRE
 
-g=9.81 # gravidade
+#tração máximas para cada helices
+helicer=[15.30818,28.14509,23.76151] #12x8 , 13x6.5 , 13x4 RIMFIRE
+
+g=9.80665 # gravidade
 p=1.225#
 s=0.454# área 
 clmax=0.561495#
@@ -24,11 +26,11 @@ def calculo(w1,t): # calculo do Sl0
 	temp=((1.44*w1*w1)/(g*p*s*clmax*(t-(d+u*(w1-l)))))
 	return(temp)
     
-for i in range(10,50):
+for i in range(10,100):
 	w.append(i) # de 10N ate 50N	
 
 for j in range(0,3): # diz qual helice 
-	for i in range(0,40):
+	for i in range(0,90):
 		if j == 0:
 			sl01.append(calculo(w[i],helicer[j]))
 		if j == 1:
@@ -37,9 +39,10 @@ for j in range(0,3): # diz qual helice
  			sl03.append(calculo(w[i],helicer[j]))
  			
 ax = plt.subplot(111)
-plt.title('Rimfire, 70% tração máxima')
-plt.ylim(0,58); # limitando o grafico até 58m
-
+#plt.title('Rimfire, 70% tração máxima')
+plt.title('Rimfire, 100% tração máxima')
+plt.ylim(0,70); # limitando o grafico até 58m
+plt.xlim(0,60)
 plt.plot(w,sl01,label='Helice: 12x8, v0=10m/s')
 plt.plot(w,sl02,label='Helice: 13x6.5, v0=10m/s')
 plt.plot(w,sl03,label='Helice: 13x4, v0=10m/s')
